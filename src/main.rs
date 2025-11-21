ab7b7edca6910e43f8639a91ab5ee7773cf25b57
tk109o <otani199910969@gmail.com>
2025-11-21 16:11:42 +0900

    health check api

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 use std::net::{Ipv4Addr, SocketAddr};
-
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{routing::get, Router, http::StatusCode};
 use tokio::net::TcpListener;
 
-// ハンドラと呼ばれる。
-async fn hello_world() -> &'static str {
-    "Hello, World!"
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
 
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/hello", get(hello_world));
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health_check", get(health_check));
     let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 8080));
-    let listener = TcpListener::bind(addr).await.unwrap();
+
+    // await? とすることで下記を簡潔に書ける
+    // 但し、 Result とセットで使う。
+    // let listener = match TcpListener::bind(addr).await {
+    //     Ok(l) => l,
+    //     Err(e) => return Err(e.into()),
+    // };
+    let listener = TcpListener::bind(addr).await?;
 
     println!("Listening on {}", addr);
 
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
