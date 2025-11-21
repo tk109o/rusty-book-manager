579f570be7a22e14bb873dd84de705cf9c0a36d3
tk109o <otani199910969@gmail.com>
2025-11-21 15:51:54 +0900

    hello world api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+// ハンドラと呼ばれる。
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello", get(hello_world));
+    let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 8080));
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on {}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
