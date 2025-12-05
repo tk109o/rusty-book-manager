ac48be3acc0234dd06f88b3aa58f2a9b796288a3
tk109o <otani199910969@gmail.com>
2025-12-05 18:04:28 +0900

    api実装 curl確認成功

---
+++ api/src/route/book.rs
@@ -0,0 +1,16 @@
+use axum::{
+    Router,
+    routing::{get, post},
+};
+use registry::AppRegistry;
+
+use crate::handler::book::{register_book, show_book, show_book_list};
+
+pub fn build_book_routers() -> Router<AppRegistry> {
+    let books_routers = Router::new()
+        .route("/", post(register_book))
+        .route("/", get(show_book_list))
+        .route("/:book_id", get(show_book));
+
+    Router::new().nest("/books", books_routers)
+}
